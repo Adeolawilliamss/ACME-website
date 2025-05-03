e4d474ffa391d2ef9bfac989a4663f77b13dc5bd
Adeolawiilliamss <babagenerah@gmail.com>
2025-05-03 21:52:34 +0100

    Updated package.json

--- ACME-website/next.config.js
+++ ACME-website/next.config.js
@@ -1,19 +1,41 @@
 // next.config.js
-const path = require('path');
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'acme-website-115r.onrender.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "acme-website-115r.onrender.com",
+        pathname: "/**",
       },
     ],
   },
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vercel.app *.googleapis.com;
+              style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+              font-src 'self' fonts.gstatic.com;
+              img-src * blob: data:;
+              connect-src *;
+            `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+    ];
   },
 };
