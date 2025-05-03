b4ad8afbe4226e996ec632fdff5e4bc2e487e44a
Adeolawiilliamss <babagenerah@gmail.com>
2025-05-03 21:21:00 +0100

    changes to the config file

--- ACME-website/next.config.mjs
+++ ACME-website/next.config.mjs
@@ -1,14 +1,20 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'acme-website-115r.onrender.com',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'acme-website-115r.onrender.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+export default nextConfig;
